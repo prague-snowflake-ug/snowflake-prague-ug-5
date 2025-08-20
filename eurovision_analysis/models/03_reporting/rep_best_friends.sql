3af1acdff5834b5ec167e0d8a0a9154ab655554b
prague-snowflake-ug <jdifko@csas.cz>
2025-08-20 17:07:13 +0000

    add new sql file

---
+++ eurovision_analysis/models/03_reporting/rep_best_friends.sql
@@ -0,0 +1,13 @@
+select
+  a.from_country_id,
+  nf.country_name as from_country,
+  a.to_country_id,
+  nt.country_name as to_country,
+  a.years_observed,
+  round(a.affinity_z_avg, 2) as affinity
+from DEV.marts.mart_country_affinities a
+left join DEV.staging.stg_eurovision__countries nf on nf.country_id = a.from_country_id
+left join DEV.staging.stg_eurovision__countries nt on nt.country_id = a.to_country_id
+where a.years_observed >= 3
+order by affinity desc
+limit 20
